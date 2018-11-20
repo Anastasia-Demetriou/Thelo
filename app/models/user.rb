e91a87b47b107803d7d1c3eef8f400ce633fe912
tttiff <charles.tiffany.r@gmail.com>
2018-11-20 12:17:36 +0000

    bugs fixed, Phelim helped correct the models, migrations, seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :user_services, dependant: :destroy
   has_many :services, through: :user_services
 
+
   validates_presence_of :first_name, :last_name, :password
   validates :email, uniqueness: { case_sensitive: false }
   # Include default devise modules. Others available are:
