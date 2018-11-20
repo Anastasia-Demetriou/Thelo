5fceda5fcda152711fdd9748bfec7e24157a810e
tttiff <charles.tiffany.r@gmail.com>
2018-11-20 19:30:53 +0000

    nav bar was covering up the top of the page so made it sticky so that it does not cover content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   #before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @users = policy_scope(User).order(created_at: :desc)
+  end
+
   def show
     @user = User.find(params[:id])
     authorize @user
   end
 
-  def index
-  end
+
 
   private
 
