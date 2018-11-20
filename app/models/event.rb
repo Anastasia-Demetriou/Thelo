0e2d13f6e8789cce477148b2e7a33a548d4f3330
tttiff <charles.tiffany.r@gmail.com>
2018-11-20 14:39:08 +0000

    updated pundit in events controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ApplicationRecord
-  has_many :bids
-  belongs_to :user
+  # has_many :bids
+  # belongs_to :user
 
-  belongs_to :service
+  # belongs_to :service
 
-  validates :name, :date, :event_type, :location, :description, :party_size, :min_price, :max_price, presence: true
+  # validates_presence_of :name, :date, :event_type, :location, :description, :party_size, :min_price, :max_price
 
 end
