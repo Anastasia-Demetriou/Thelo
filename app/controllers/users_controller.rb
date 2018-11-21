57565e7a0d327216cb66f8436df88b24f2cd3523
tttiff <charles.tiffany.r@gmail.com>
2018-11-21 08:46:50 +0000

    updated user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,46 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @events = []
+    @user.events.each do |event|
+      @events << event
+    end
+
+    # @professional_bids_accepted = []
+    # @user.bid.accepted
+
+    # @event_bids_accepted = []
+    # @user.event.bid.accepted
+
+
+
+    # @bids = []
+    # @user.bids.each do |bid|
+    #   @bids << bid.event
+    # end
+
+
     authorize @user
   end
 
 
 
   private
+
+  def professional?
+    @user = current_user.professional
+  end
+
+  def accepted
+    @user.bid.accepted == true
+  end
+
+  # def bookings
+  #   @bookings = []
+  #   @events.confirmed_bid.each do |booking|
+  #     @bookings << booking
+  #   end
+  # end
 
   # def set_user
   #   @user = User.find(params[:id])
