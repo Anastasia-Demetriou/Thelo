6f767706489cef0ce727aba5b43d6fe0abd75a44
tttiff <charles.tiffany.r@gmail.com>
2018-11-23 13:54:54 +0000

    updated sign up form and edit form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
 
  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :professional])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :professional, :service_ids])
 end
 
 end
