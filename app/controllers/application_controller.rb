0f92af14e52d085c943d0b5b38b2c6c94e9de627
tttiff <charles.tiffany.r@gmail.com>
2018-11-30 11:59:10 +0000

    can now edit photos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,7 @@ class ApplicationController < ActionController::Base
  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :professional, :service_ids, :photo])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :professional, :service_ids, :photo])
 end
 
 def default_url_options
